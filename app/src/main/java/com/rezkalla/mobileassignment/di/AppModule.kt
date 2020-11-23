c8ab39859d4fc610519dff07050bf6b18668c330
Mina Rezkalla <minashakeralfy@gmail.com>
2020-11-23 13:25:13 +0100

    add App module

---
+++ app/src/main/java/com/rezkalla/mobileassignment/di/AppModule.kt
@@ -0,0 +1,16 @@
+package com.rezkalla.mobileassignment.di
+
+import android.app.Application
+import android.content.Context
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+@Module
+class AppModule {
+
+    @Provides
+    @Singleton
+    fun provideContext(application: Application) : Context = application.applicationContext
+
+}
