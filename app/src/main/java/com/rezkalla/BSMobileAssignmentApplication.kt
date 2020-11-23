c7ce090fd9f23739037eee5f6d18960975bf804b
Mina Rezkalla <minashakeralfy@gmail.com>
2020-11-23 13:30:02 +0100

    connect all dependencies in the application

--- app/src/main/java/com/rezkalla/BSMobileAssignmentApplication.kt
+++ app/src/main/java/com/rezkalla/BSMobileAssignmentApplication.kt
@@ -1,6 +1,33 @@
 package com.rezkalla
 
 import android.app.Application
+import com.rezkalla.mobileassignment.di.DaggerAppComponent
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasAndroidInjector
+import javax.inject.Inject
 
-class BSMobileAssignmentApplication :Application()  {
+class BSMobileAssignmentApplication :Application() ,HasAndroidInjector  {
+
+    @Inject
+    lateinit var androidInjector: DispatchingAndroidInjector<Any>
+
+
+    override fun androidInjector(): AndroidInjector<Any> {
+        return androidInjector
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        initDagger()
+    }
+
+
+    private fun initDagger() {
+        DaggerAppComponent
+            .builder()
+            .application(this)
+            .build()
+            .inject(this)
+    }
 }
