716bed4daab3de99162ae2b5ffb82974aa23a8ad
Mina Rezkalla <minashakeralfy@gmail.com>
2020-11-25 21:13:09 +0100

    Filter using Default Dispatchers

--- app/src/main/java/com/rezkalla/mobileassignment/presentation/CitiesViewModel.kt
+++ app/src/main/java/com/rezkalla/mobileassignment/presentation/CitiesViewModel.kt
@@ -2,9 +2,14 @@ package com.rezkalla.mobileassignment.presentation
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.rezkalla.core.domain.usecase.FilterCitiesUseCase
 import com.rezkalla.mobileassignment.model.City
 import com.rezkalla.mobileassignment.model.mapper.CityMapper
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class CitiesViewModel @Inject constructor(
@@ -15,9 +20,16 @@ class CitiesViewModel @Inject constructor(
     val citiesLiveData = MutableLiveData<List<City>>()
 
     fun getCities(word: String = "") {
+        viewModelScope.launch {
+          filter(word)
+        }
+    }
+
+    private suspend fun filter(word: String) = withContext(Dispatchers.Default) {
         val cities = getCitiesUseCase(word).map {
             mapper.from(it)
         }
         citiesLiveData.postValue(cities)
     }
+
 }
